ae0313b424e154c87113f1f072fc1cf6ed4ae76d
Ahmed Mamdouh <Ahmed.Mamdouh@linkdev.com>
2024-09-13 23:21:45 +0300

    Adding the change ticker color service

---
+++ Application/Common/Interfaces/IChangeTicketColor.cs
@@ -0,0 +1,8 @@
+﻿using Talabeyah.TicketManagement.Domain.Enums;
+
+namespace Talabeyah.TicketManagement.Application.Common.Interfaces;
+
+public interface IChangeTicketColor
+{
+      Task ChangeTicketColourAsync(int ticketId, Color color);
+}
---
+++ Application/Common/Services/ChangeTicketColor.cs
@@ -0,0 +1,41 @@
+﻿using Hangfire;
+using Talabeyah.TicketManagement.Application.Common.Interfaces;
+using Talabeyah.TicketManagement.Application.Common.Repositories;
+using Talabeyah.TicketManagement.Domain.Enums;
+
+namespace Talabeyah.TicketManagement.Application.Common.Services;
+
+public class ChangeTicketColor : IChangeTicketColor
+{
+    private readonly ITicketRepository _repository;
+    private readonly IBackgroundJobClient _backgroundJobClient;
+
+    public ChangeTicketColor(ITicketRepository repository, IBackgroundJobClient backgroundJobClient)
+    {
+        _repository = repository;
+        _backgroundJobClient = backgroundJobClient;
+    }
+
+    //this method must be public for the hangfire to work
+    // separate it from the Handle method to application/services
+    [AutomaticRetry(Attempts = 0)]
+    public async Task ChangeTicketColourAsync(int ticketId, Color color)
+    {
+        var ticket = await _repository.GetByIdAsync(ticketId);
+
+        //I want to do this and return so no need for the next operations or checks
+        if (color is Color.Red)
+        {
+            ticket.HandleTicket();
+            await _repository.UpdateAsync(ticket, CancellationToken.None);
+            return;
+        }
+        
+        ticket.ChangeColour(color);
+        if (ticket.Colour != Color.None)
+        {
+            _backgroundJobClient.Schedule(() => ChangeTicketColourAsync(ticketId, ticket.Colour), TimeSpan.FromMinutes(15));
+        }
+        await _repository.UpdateAsync(ticket, CancellationToken.None);
+    }
+}
