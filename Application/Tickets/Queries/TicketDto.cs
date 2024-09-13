d3704d110acdd5ae0ed34c3fbddfa91afecd3d7f
Ahmed Mamdouh <Ahmed.Mamdouh@linkdev.com>
2024-09-13 14:51:02 +0300

    Adding Tickcet Dto

---
+++ Application/Tickets/Queries/TicketDto.cs
@@ -0,0 +1,22 @@
+﻿using Domain.TicketAggregate.Entities;
+using Domain.TicketAggregate.Enums;
+using Domain.TicketAggregate.ValueObjects;
+
+namespace Application.Tickets.Queries;
+
+public class TicketDto
+{
+    public int Id { get; set; }
+    public string PhoneNumber { get; set; }
+    public Location Location { get; set; }
+    public bool IsHandled { get; set; }
+    public Colour Colour { get; set; }
+    
+    private class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<Ticket, TicketDto>();
+        }
+    }
+}
