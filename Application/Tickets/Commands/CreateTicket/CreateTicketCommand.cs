670bb1cbc831bdeb3fe4090bcca862e871dbc539
Ahmed Mamdouh <Ahmed.Mamdouh@linkdev.com>
2024-09-13 14:53:04 +0300

    Adding create ticket command and its validator

---
+++ Application/Tickets/Commands/CreateTicket/CreateTicketCommand.cs
@@ -0,0 +1,37 @@
+﻿using Application.Common.Interfaces;
+using Domain.TicketAggregate;
+using Domain.TicketAggregate.Entities;
+using Domain.TicketAggregate.Events;
+using Domain.TicketAggregate.Services;
+using Domain.TicketAggregate.ValueObjects;
+
+namespace Application.Tickets.Commands.CreateTicket;
+
+public record CreateTicketCommand : IRequest<int>
+{
+    public string PhoneNumber { get; set; }
+    public Location Location { get; set; }
+}
+
+public class Handler : IRequestHandler<CreateTicketCommand, int>
+{
+    private readonly ITicketService _ticketService;
+    private readonly IValidator<CreateTicketCommand> _validator;
+
+    public Handler(ITicketService ticketService, IValidator<CreateTicketCommand> validator)
+    {
+        _ticketService = ticketService;
+        _validator = validator;
+    }
+
+    public async Task<int> Handle(CreateTicketCommand request, CancellationToken cancellationToken)
+    {
+        var validationResult = await _validator.ValidateAsync(request, cancellationToken);
+        if (!validationResult.IsValid)
+        {
+            throw new ValidationException(validationResult.Errors);
+        }
+
+        return await _ticketService.CreateTicketAsync(request.PhoneNumber, request.Location, cancellationToken);
+    }
+}
---
+++ Application/Tickets/Commands/CreateTicket/CreateTicketCommandValidator.cs
@@ -0,0 +1,26 @@
+﻿using Application.Tickets.Commands.CreateTicket;
+
+namespace Application.Tickets.Commands.CreateTicket;
+
+public class CreateTicketCommandValidator : AbstractValidator<CreateTicketCommand>
+{
+    public CreateTicketCommandValidator()
+    {
+        RuleFor(x => x.PhoneNumber)
+            .NotEmpty().WithMessage("Phone number is required.")
+            .Matches(@"^\+?[1-9]\d{1,14}$").WithMessage("Phone number is not valid.");
+
+        RuleFor(x => x.Location)
+            .NotNull().WithMessage("Location is required.");
+
+        RuleFor(x => x.Location.Governorate)
+            .NotEmpty().WithMessage("Governorate is required.");
+
+        RuleFor(x => x.Location.City)
+            .NotEmpty().WithMessage("City is required.");
+
+        RuleFor(x => x.Location.District)
+            .NotEmpty().WithMessage("District is required.");
+        
+    }
+}
