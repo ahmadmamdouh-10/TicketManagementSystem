f7c13711d2d47188b865bde9a156a959235663f9
Ahmed Mamdouh <Ahmed.Mamdouh@linkdev.com>
2024-09-13 14:53:30 +0300

    adding tickets listing with pagination query

---
+++ Application/Tickets/Queries/GetTicketsWithPaginationQuery.cs
@@ -0,0 +1,34 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Mappings;
+using Application.Common.Models;
+
+namespace Application.Tickets.Queries;
+
+public record GetTicketsWithPaginationQuery : IRequest<PaginatedList<TicketDto>>
+{
+    public int ListId { get; init; }
+    public int PageNumber { get; init; } = 1;
+    public int PageSize { get; init; } = 10;
+}
+
+public class GetTodoItemsWithPaginationQueryHandler : IRequestHandler<GetTicketsWithPaginationQuery,
+    PaginatedList<TicketDto>>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public GetTodoItemsWithPaginationQueryHandler(IApplicationDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<PaginatedList<TicketDto>> Handle(GetTicketsWithPaginationQuery request,
+        CancellationToken cancellationToken)
+    {
+        return await _context.Tickets
+            .OrderBy(x => x.Created)
+            .ProjectTo<TicketDto>(_mapper.ConfigurationProvider)
+            .PaginatedListAsync(request.PageNumber, request.PageSize);
+    }
+}
