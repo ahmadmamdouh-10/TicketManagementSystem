62e5290d0c7c5da5780c83abc2993b7b14c03711
Ahmed Mamdouh <Ahmed.Mamdouh@linkdev.com>
2024-09-13 14:50:37 +0300

    Adding Application Services
    Adding Application Services (hangfire/mediatR/AutoMapper/and all pipelines behaviors) using Reflection

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using Application.Common.Behaviours;
+using Hangfire;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services
+        , IConfiguration configuration)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(PerformanceBehaviour<,>));
+        });
+
+        var connectionString = configuration.GetConnectionString("HangfireConnection");
+
+
+        // Add Hangfire services
+        services.AddHangfire(config => config.UseSqlServerStorage(connectionString));
+        services.AddHangfireServer();
+
+        return services;
+    }
+}
