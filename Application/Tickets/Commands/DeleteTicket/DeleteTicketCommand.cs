d8dba1012797fcb0785f91c2c0db8b758c44a8f8
Ahmed Mamdouh <Ahmed.Mamdouh@linkdev.com>
2024-09-13 14:55:35 +0300

    adding delete ticket command and its validator

---
+++ Application/Tickets/Commands/DeleteTicket/DeleteTicketCommand.cs
@@ -0,0 +1,31 @@
+﻿using Application.Common.Interfaces;
+using Domain.TicketAggregate.Events;
+using Domain.TicketAggregate.Services;
+
+namespace Application.Tickets.Commands.DeleteTicket;
+
+public record DeleteTicketCommand(int Id) : IRequest;
+
+public class DeleteTicketCommandHandler : IRequestHandler<DeleteTicketCommand>
+{
+    private readonly ITicketService _ticketService;
+    private readonly IValidator<DeleteTicketCommand> _validator;
+
+
+    public DeleteTicketCommandHandler(ITicketService ticketService, IValidator<DeleteTicketCommand> validator)
+    { 
+        _ticketService = ticketService;
+        _validator = validator;
+    }
+
+    public async Task Handle(DeleteTicketCommand request, CancellationToken cancellationToken)
+    {
+        var validationResult = await _validator.ValidateAsync(request, cancellationToken);
+        if (!validationResult.IsValid)
+        {
+            throw new ValidationException(validationResult.Errors);
+        }
+        
+        await _ticketService.DeleteTicketAsync(request.Id, cancellationToken);
+    }
+}
---
+++ Application/Tickets/Commands/DeleteTicket/DeleteTicketCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Tickets.Commands.DeleteTicket;
+
+
+public class DeleteTicketCommandValidator : AbstractValidator<DeleteTicketCommand>
+{
+    public DeleteTicketCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Id is required.");
+    }
+}
