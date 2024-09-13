052b7b3e79cfdd427949b5738837a61e3b018505
Ahmed Mamdouh <Ahmed.Mamdouh@linkdev.com>
2024-09-13 14:43:16 +0300

    Adding ValueObjects for Location and PhoneNumber with their validations

---
+++ Domain/TicketAggregate/ValueObjects/Location.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Common;
+
+namespace Domain.TicketAggregate.ValueObjects;
+
+public class Location : ValueObject
+{
+    public string Governorate { get; }
+    public string City { get; }
+    public string District { get; }
+
+    public Location(string governorate, string city, string district)
+    {
+        Governorate = governorate;
+        City = city;
+        District = district;
+    }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Governorate;
+        yield return City;
+        yield return District;
+    }
+}
---
+++ Domain/TicketAggregate/ValueObjects/PhoneNumber.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.RegularExpressions;
+using Domain.Common;
+
+namespace Domain.TicketAggregate.ValueObjects;
+
+
+public class PhoneNumber : ValueObject
+{
+    public string Value { get; }
+
+    private PhoneNumber(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new ArgumentException("Phone number cannot be empty", nameof(value));
+
+        if (!IsValidPhoneNumber(value))
+            throw new ArgumentException("Invalid phone number format", nameof(value));
+
+        Value = value;
+    }
+
+    public static PhoneNumber Create(string value) => new PhoneNumber(value);
+
+    private static bool IsValidPhoneNumber(string phoneNumber)
+    {
+        return Regex.IsMatch(phoneNumber, @"^\+?[1-9]\d{1,14}$");
+    }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
