d7dadff2ec48649d5e69155169ef91246f4af819
Ahmed Mamdouh <Ahmed.Mamdouh@linkdev.com>
2024-09-13 14:52:12 +0300

    Separate DbContext for readonly queries from write DbContext for scalability and performance wise

---
+++ Infrastructure/ReadOnlyApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using Application.Common.Interfaces;
+using Domain.TicketAggregate.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure;
+
+public class ReadOnlyApplicationDbContext : DbContext, IApplicationDbContext
+{
+    public ReadOnlyApplicationDbContext(DbContextOptions<ReadOnlyApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Ticket> Tickets => Set<Ticket>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
