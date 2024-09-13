093b5245994e6bcd8fad44b8d0c0b37da4b653cc
Ahmed Mamdouh <Ahmed.Mamdouh@linkdev.com>
2024-09-13 14:12:55 +0300

    Adding shard/common Guard to check Invalid Numbers and Nulls

---
+++ Domain/Common/Guard.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Common;
+
+public static class Guard
+{
+    public static void AgainstNull(object value, string parameterName)
+    {
+        if (value == null)
+            throw new ArgumentNullException(parameterName);
+    }
+
+    public static void AgainstInvalidAmount(decimal amount, string parameterName)
+    {
+        if (amount <= 0)
+            throw new ArgumentException("Amount must be greater than zero", parameterName);
+    }
+}
