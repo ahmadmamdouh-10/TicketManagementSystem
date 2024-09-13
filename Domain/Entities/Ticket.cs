aabef749e10e41fde394e6c40e5cbf66108a4ec3
Ahmed Mamdouh <Ahmed.Mamdouh@linkdev.com>
2024-09-13 23:15:12 +0300

    change the place of ticket entity to be in the entities folder and update the naming of color on it and add the change color business logic to it

---
+++ Domain/Entities/Ticket.cs
@@ -0,0 +1,63 @@
+﻿using Talabeyah.TicketManagement.Domain.Common;
+using Talabeyah.TicketManagement.Domain.Enums;
+using Talabeyah.TicketManagement.Domain.Events;
+using Talabeyah.TicketManagement.Domain.ValueObjects;
+
+namespace Talabeyah.TicketManagement.Domain.Entities;
+
+public class Ticket : BaseAuditableEntity, IAgreggateRoot
+{
+    public PhoneNumber PhoneNumber { get; private set; }
+    public Location Location { get; private set; }
+    public bool IsHandled { get; private set; }
+    public Color Colour { get; private set; }
+
+    // Private constructor to enforce the use of the factory method
+    private Ticket()
+    {
+        Colour = Color.None;
+        IsHandled = false;
+    }
+    public void HandleTicket()
+    {
+        if (IsHandled)
+            throw new InvalidOperationException("Ticket is already handled");
+
+        IsHandled = true;
+        AddDomainEvent(new TicketHandledEvent(this));
+    }
+
+    public void ChangeColour(Color newColor)
+    {
+        Color nextColor = newColor switch
+        {
+            Color.None => Color.Yellow,
+            Color.Yellow => Color.Green,
+            Color.Green => Color.Blue,
+            Color.Blue => Color.Red,
+            _ => Color.None
+        };
+        if (nextColor == Color.None)
+            throw new ArgumentException("Invalid colour", nameof(newColor));
+
+        Colour = newColor;
+        AddDomainEvent(new TicketColourChangedEvent(this));
+    }
+
+    public void Delete()
+    {
+        AddDomainEvent(new TicketDeletedEvent(this));
+    }
+
+    // Static factory method to create a Ticket and add the domain event
+    public static Ticket Create(string phoneNumber, Location location)
+    {
+        var ticket = new Ticket
+        {
+            PhoneNumber = PhoneNumber.Create(phoneNumber),
+            Location = location
+        };
+        ticket.AddDomainEvent(new TicketCreatedEvent(ticket));
+        return ticket;
+    }
+}
