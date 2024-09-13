fc2cda3941cb47594e974730f910a1c25205f088
Ahmed Mamdouh <Ahmed.Mamdouh@linkdev.com>
2024-09-13 23:11:15 +0300

    Apply SOLID to the repository and remove unneeded remove business logic from it.

--- Infrastructure/Repositories/TicketRepository.cs
+++ Infrastructure/Repositories/TicketRepository.cs
@@ -1,52 +1,94 @@
-﻿using Application.Common.Interfaces;
-using Domain.TicketAggregate.Entities;
-using Domain.TicketAggregate.Enums;
-using Domain.TicketAggregate.Events;
-using Domain.TicketAggregate.Repositories;
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using Talabeyah.TicketManagement.Application.Common.Mappings;
+using Talabeyah.TicketManagement.Application.Common.Models;
+using Talabeyah.TicketManagement.Application.Common.Repositories;
+using Talabeyah.TicketManagement.Application.Tickets.Queries;
+using Talabeyah.TicketManagement.Domain.Entities;
+using Talabeyah.TicketManagement.Domain.Enums;
 
-namespace Infrastructure.Repositories;
+namespace Talabeyah.TicketManagement.Infrastructure.Repositories;
 
 public class TicketRepository : ITicketRepository
 {
-    private readonly IApplicationDbContext _context;
+    private readonly ApplicationDbContextReadOnly _applicationDbContextReadOnlyDbContext;
+    private readonly ApplicationDbContext _dbContext;
+    private readonly IMapper _mapper;
 
-    public TicketRepository(IApplicationDbContext context)
+
+    public TicketRepository(ApplicationDbContextReadOnly applicationDbContextReadOnlyDbContext,
+        ApplicationDbContext dbContext,
+        IMapper mapper)
     {
-        _context = context;
+        _mapper = mapper;
+        _applicationDbContextReadOnlyDbContext = applicationDbContextReadOnlyDbContext;
+        _dbContext = dbContext;
     }
-
+    
     public async Task<Ticket> GetByIdAsync(int id)
     {
-        return await _context.Tickets.FindAsync(id);
+        return await _dbContext.Tickets.FindAsync(new object[] { id });
     }
 
-    public async Task AddAsync(Ticket ticket)
+    public async Task<PaginatedList<TicketDto>> GetTicketsAsync(int pageNumber, int pageSize)
     {
-        await _context.Tickets.AddAsync(ticket);
+        return await _applicationDbContextReadOnlyDbContext.Tickets
+            .OrderBy(x => x.Created)
+            .ProjectTo<TicketDto>(_mapper.ConfigurationProvider)
+            .PaginatedListAsync(pageNumber, pageSize);
     }
 
-    public Task DeleteAsync(Ticket ticket)
+    public async Task<int> AddAsync(Ticket ticket, CancellationToken cancellationToken)
     {
-        _context.Tickets.Remove(ticket);
+        await _dbContext.Tickets.AddAsync(ticket, cancellationToken);
+        return await _dbContext.SaveChangesAsync(cancellationToken);
+    }
+    
+    public async Task<int> UpdateAsync(Ticket ticket, CancellationToken cancellationToken)
+    {
+        _dbContext.Entry(ticket).State = EntityState.Modified;
+        return await _dbContext.SaveChangesAsync(cancellationToken);
+    }
+    
+    public async Task<int> DeleteAsync(Ticket ticket, CancellationToken cancellationToken)
+    {
+        _dbContext.Tickets.Remove(ticket);
+        return await _dbContext.SaveChangesAsync(cancellationToken);
+    }
+
+    //it should follow Single Responsibility Principle
+    public async Task<int> DeleteAsync(int id, CancellationToken cancellationToken)
+    {
+        var ticket = await _dbContext.Tickets.FindAsync(id);
+        if (ticket == null)
+        {
+            throw new KeyNotFoundException($"Ticket with id {id} not found.");
+        }
+
+        _dbContext.Tickets.Remove(ticket);
         ticket.Delete();
-        return Task.CompletedTask;
+        return await _dbContext.SaveChangesAsync(cancellationToken);
     }
 
-    public async Task<Ticket> ChangeTicketColourAsync(int id, Colour colour, CancellationToken cancellationToken)
+    //it shouldn't be here, as it is a command
+    public async Task<Ticket> ChangeTicketColourAsync(int id, Color colour, CancellationToken cancellationToken)
     {
-        var ticket = await _context.Tickets.FindAsync(new object[] { id }, cancellationToken);
+        var ticket = await _dbContext.Tickets.FindAsync(new object[] { id }, cancellationToken);
         if (ticket == null)
         {
             throw new KeyNotFoundException($"Ticket with id {id} not found.");
         }
 
         ticket.ChangeColour(colour);
+        await _dbContext.SaveChangesAsync(cancellationToken);
         return ticket;
     }
 
+    //it shouldn't be here, as it is a command
     public async Task<Ticket> HandleTicketAsync(int id, CancellationToken cancellationToken)
     {
-        var ticket = await _context.Tickets.FindAsync(new object[] { id }, cancellationToken);
+        var ticket = await _dbContext.Tickets.FindAsync(new object[] { id }, cancellationToken);
         if (ticket == null)
         {
             throw new KeyNotFoundException($"Ticket with id {id} not found.");
@@ -57,6 +99,7 @@ public class TicketRepository : ITicketRepository
             ticket.HandleTicket();
         }
 
+        await _dbContext.SaveChangesAsync(cancellationToken);
         return ticket;
     }
 }
