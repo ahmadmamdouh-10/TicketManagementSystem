6b9a031b767d9a505c58e31d1184affd4e1a03e7
Ahmed Mamdouh <Ahmed.Mamdouh@linkdev.com>
2024-09-13 14:55:17 +0300

    Adding change ticket priority color command and its validator

---
+++ Application/Tickets/Commands/ChangeTicketColour/ChangeTicketColour.cs
@@ -0,0 +1,35 @@
+﻿using Application.Common.Interfaces;
+using Domain.TicketAggregate.Enums;
+using Domain.TicketAggregate.Events;
+using Domain.TicketAggregate.Services;
+
+namespace Application.Tickets.Commands.ChangeTicketColour;
+
+public record ChangeTicketColour : IRequest
+{
+    public int Id { get; init; }
+    public Colour Colour { get; init; }
+}
+
+public class ChangeTicketColourHandler : IRequestHandler<ChangeTicketColour>
+{
+    private readonly ITicketService _ticketService;
+    private readonly IValidator<ChangeTicketColour> _validator;
+
+    public ChangeTicketColourHandler(ITicketService ticketService, IValidator<ChangeTicketColour> validator)
+    {
+        _ticketService = ticketService;
+        _validator = validator;
+    }
+
+    public async Task Handle(ChangeTicketColour request, CancellationToken cancellationToken)
+    {
+        var validationResult = await _validator.ValidateAsync(request, cancellationToken);
+        if (!validationResult.IsValid)
+        {
+            throw new ValidationException(validationResult.Errors);
+        }
+
+        await _ticketService.HandleTicketAsync(request.Id, cancellationToken);
+    }
+}
---
+++ Application/Tickets/Commands/ChangeTicketColour/ChangeTicketColourValidator.cs
@@ -0,0 +1,14 @@
+﻿namespace Application.Tickets.Commands.ChangeTicketColour;
+
+public class ChangeTicketColourValidator : AbstractValidator<ChangeTicketColour>
+{
+    public ChangeTicketColourValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Id is required.");
+        
+        RuleFor(v => v.Colour)
+            .NotEmpty().WithMessage("Invalid colour.");
+
+    }
+}
