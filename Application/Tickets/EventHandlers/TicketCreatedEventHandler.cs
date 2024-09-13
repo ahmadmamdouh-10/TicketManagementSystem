47f799c76756705ffd80b9f45f749d9c471798af
Ahmed Mamdouh <Ahmed.Mamdouh@linkdev.com>
2024-09-13 23:20:23 +0300

    Update the TicketCreatedEventHandler.cs to have _backgroundJobClient and schedule event handler to updates the color of the ticket.

--- Application/Tickets/EventHandlers/TicketCreatedEventHandler.cs
+++ Application/Tickets/EventHandlers/TicketCreatedEventHandler.cs
@@ -1,55 +1,36 @@
-﻿using Domain.TicketAggregate.Enums;
-using Domain.TicketAggregate.Events;
-using Domain.TicketAggregate.Services;
-using Hangfire;
+﻿using Hangfire;
 using Microsoft.Extensions.Logging;
+using Talabeyah.TicketManagement.Application.Common.Interfaces;
+using Talabeyah.TicketManagement.Domain.Enums;
+using Talabeyah.TicketManagement.Domain.Events;
 
-namespace Application.Tickets.EventHandlers;
+namespace Talabeyah.TicketManagement.Application.Tickets.EventHandlers;
 
 public class TicketCreatedEventHandler : INotificationHandler<TicketCreatedEvent>
 {
     private readonly ILogger<TicketCreatedEventHandler> _logger;
-    private readonly ITicketService _ticketService;
+
+    private readonly IBackgroundJobClient _backgroundJobClient;
 
 
-    public TicketCreatedEventHandler(ILogger<TicketCreatedEventHandler> logger, ITicketService ticketService)
+    public TicketCreatedEventHandler(ILogger<TicketCreatedEventHandler> logger,
+        IBackgroundJobClient backgroundJobClient)
     {
         _logger = logger;
-        _ticketService = ticketService;
+        _backgroundJobClient = backgroundJobClient;
     }
 
     public Task Handle(TicketCreatedEvent notification, CancellationToken cancellationToken)
     {
         _logger.LogInformation("Ticket Created with Id {TicketId}", notification.Ticket.Id);
 
+        //background service of hangfire
         // Schedule the first color change to yellow after 15 minutes
-        BackgroundJob.Schedule(() => ChangeTicketColour(notification.Ticket.Id, Colour.Yellow),
+        _backgroundJobClient.Schedule<IChangeTicketColor>(
+            (eventHandler) =>
+                eventHandler.ChangeTicketColourAsync(notification.Ticket.Id, Color.Yellow),
             TimeSpan.FromMinutes(15));
 
         return Task.CompletedTask;
-    }
-
-    [AutomaticRetry(Attempts = 0)]
-    private async Task ChangeTicketColour(int ticketId, Colour colour)
-    {
-        _ticketService.ChangeTicketColourAsync(ticketId, colour, CancellationToken.None).Wait();
-
-        // Schedule the next color change if applicable
-        Colour nextColour = colour switch
-        {
-            Colour.Yellow => Colour.Green,
-            Colour.Green => Colour.Red,
-            _ => Colour.None
-        };
-
-        if (nextColour != Colour.None)
-        {
-            BackgroundJob.Schedule(() => ChangeTicketColour(ticketId, nextColour), TimeSpan.FromMinutes(15));
-        }
-        else if (colour == Colour.Red)
-        {
-            // Handle the ticket when the color is red
-            await _ticketService.HandleTicketAsync(ticketId, CancellationToken.None);
-        }
     }
 }
